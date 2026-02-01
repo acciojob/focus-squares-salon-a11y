fdb09972a61dc912a1e20d6b873cea784bc2dde0
saloni mourya <manshimaurya195@gmail.com>
2026-02-02 00:32:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
 //your JS code here. If required.
+
+// let boxs = document.querySelectorAll("div")
+// for(let box of boxs ){
+// 	box.addEventListener("mouseenter" , defaultcolor)
+// 	box.addEventListener("mouseleave",changecolor)
+// }
+// function defaultcolor(e){
+	
+// 	e.target.style.backgroundColor = "#E6E6FA"
+// 	function changecolor(e){
+// 		let value = e.target;
+		
+// 			value.style.backgroundColor = "#6F4E3"
+// 	}	
+// }
+let boxs = document.querySelectorAll(".square");
+
+for (let box of boxs) {
+  box.addEventListener("mouseenter", defaultcolor);
+  // box.addEventListener("mouseleave", changecolor);
+}
+
+function defaultcolor(e) {
+	let value = e.target;
+	for(let box of boxs){
+	if(box===value){
+     box.style.backgroundColor =  "#E6E6FA";
+}
+else{
+	  box.style.backgroundColor =  "#6F4E37";
+}
+}
+}
+function changecolor(e) {
+  // e.target.style.backgroundColor = "#6F4E3A";
+}
