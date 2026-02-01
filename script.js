70ee7a2e2ab05d7e1f63f7de04d884b7b99cb8a3
saloni mourya <manshimaurya195@gmail.com>
2026-02-02 00:42:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,5 @@
 //your JS code here. If required.
 
-// let boxs = document.querySelectorAll("div")
-// for(let box of boxs ){
-// 	box.addEventListener("mouseenter" , defaultcolor)
-// 	box.addEventListener("mouseleave",changecolor)
-// }
-// function defaultcolor(e){
-	
-// 	e.target.style.backgroundColor = "#E6E6FA"
-// 	function changecolor(e){
-// 		let value = e.target;
-		
-// 			value.style.backgroundColor = "#6F4E3"
-// 	}	
-// }
 let boxs = document.querySelectorAll(".square");
 
 for (let box of boxs) {
